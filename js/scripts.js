4a51fd64ff9a8235f3fe17e59e951aaa93d5925a
Justin Brown <snakeguy2000@gmail.com>
2020-02-22 10:36:00 -0800

    Week 4 assignment

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
+$(function() {
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('toggle')
+    });
+});
+$(function() {
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('toggle')
+    });
+});
